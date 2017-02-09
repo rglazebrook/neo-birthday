e190edaa31b9be45875182534c9e60bab86c77e5
Rob Glazebrook <rglazebrook@gmail.com>
2017-02-08 20:57:59 -0600

    Cleaning up JS and comments.

--- js/main.js
+++ js/main.js
@@ -28,17 +28,16 @@ var $month = $('#month'),
 
 $(document).ready(function() {
 
-	// When the month changes, tailor the number of days.
+	// When the month changes, disable any days that aren't in that month.
 	$month.on('change', function() {
 		var days = daysInMonth[ parseInt( $(this).val() ) ];
 
 		$day.val( '01' );
 
 		$dayOptions.each( function(i, el) {
-
 			$(el).attr( 'disabled', i + 1 > days ? 'disabled' : false );
-
 		});
+
 	}).change();
 
 	// Handle form submission.
@@ -77,7 +76,6 @@ $(document).ready(function() {
 
 // Create our asteroids!
 function createAsteroids(data) {
-	console.log(data);
 
 	var count = data.element_count,
 		asteroids = data.near_earth_objects[date].sort( function(a, b) {
@@ -89,64 +87,56 @@ function createAsteroids(data) {
 	largestAsteroid = null;
 	fastestAsteroid = null;
 
-	if ( count == 0 ) {
-		// TODO: Handle no objects.
-	}
+	// Set the title info.
+	var plural = count > 1;
+	$countTitle.find('#count').text( count );
+	$countTitle.find('#waswere').text( plural ? 'were' : 'was' );
+	$countTitle.find('#plural').text( plural ? 's' : '');
 
-	else {
+	// Find the largest/fastest asteroids, so we can use them to scale the others.
+	findLargestAsteroid( asteroids );
+	findFastestAsteroid( asteroids );
 
-		// Set the title info.
-		var plural = count > 1;
-		$countTitle.find('#count').text( count );
-		$countTitle.find('#waswere').text( plural ? 'were' : 'was' );
-		$countTitle.find('#plural').text( plural ? 's' : '');
+	// Create all of our asteroids and their associated tooltip information.
+	for ( var i=0; i < count; i++ ) {
 
-		// Find the largest asteroid, so we can use it to scale the others.
-		findLargestAsteroid( asteroids );
-		findFastestAsteroid( asteroids );
+		var asteroid = asteroids[i],
+			$obj = $('<div class="object"><div class="asteroid tooltip" data-tooltip-content="#asteroid-'+i+'"></div></div>'),
+			$tooltip = $('<div id="asteroid-'+i+'"><h5 class="name text-center"></h5><p><strong>Diameter:</strong> <span class="min"></span> - <span class="max"></span> feet</p><p><strong>Closest Approach:</strong> <span class="distance"></span> miles</p><p><strong>Velocity at Approach:</strong> <span class="vel"></span> MPH</p></div>'),
+			diaMax = asteroid.estimated_diameter.feet.estimated_diameter_max,
+			widthPercent = ( diaMax / largestAsteroid * 100 ),
+			marginPercent = (100 - widthPercent) / 2,
+			velRatio = fastestAsteroid / asteroid.close_approach_data[0].relative_velocity.miles_per_hour;
+			$objAsteroid = $obj.find('.asteroid');
 
-		// Create the appropriate number of asteroids.
-		for ( var i=0; i < count; i++ ) {
+		// Size our asteroid.
+		// Asteroids are all relatively sized (which can create some tiny asteroids, but it's still an interesting visual).
+		$objAsteroid.css({ 
+			width: widthPercent + '%',
+			paddingBottom: widthPercent + '%',
+			margin: marginPercent + '% auto'
+		});
 
-			var asteroid = asteroids[i],
-				$obj = $('<div class="object"><div class="asteroid tooltip" data-tooltip-content="#asteroid-'+i+'"></div></div>'),
-				$tooltip = $('<div id="asteroid-'+i+'"><h5 class="name text-center"></h5><p><strong>Diameter:</strong> <span class="min"></span> - <span class="max"></span> feet</p><p><strong>Closest Approach:</strong> <span class="distance"></span> miles</p><p><strong>Velocity at Approach:</strong> <span class="vel"></span> MPH</p></div>'),
-				diaMin = asteroid.estimated_diameter.feet.estimated_diameter_min,
-				diaMax = asteroid.estimated_diameter.feet.estimated_diameter_max,
-				widthPercent = ( diaMax / largestAsteroid * 100 ),
-				marginPercent = (100 - widthPercent) / 2,
-				velRatio = fastestAsteroid / asteroid.close_approach_data[0].relative_velocity.miles_per_hour;
-				$objAsteroid = $obj.find('.asteroid');
+		// Populate our tooltip.
+		$tooltip.find('.name').text( asteroid.name );
+		$tooltip.find('.min').text( Math.round(asteroid.estimated_diameter.feet.estimated_diameter_min) );
+		$tooltip.find('.max').text( Math.round(asteroid.estimated_diameter.feet.estimated_diameter_max) );
+		$tooltip.find('.vel').text( Math.round(asteroid.close_approach_data[0].relative_velocity.miles_per_hour) );
+		$tooltip.find('.distance').text( Math.round(asteroid.close_approach_data[0].miss_distance.miles) );
 
-			// Size our asteroid.
-			$objAsteroid.css({ 
-				width: widthPercent + '%',
-				paddingBottom: widthPercent + '%',
-				margin: marginPercent + '% auto'
-			});
+		// Add our new asteroid and tooltip to the DOM.
+		$objects.append( $obj );
+		$tooltips.append( $tooltip );
 
-			// Populate our tooltip.
-			$tooltip.find('.name').text( asteroid.name );
-			$tooltip.find('.min').text( Math.round(asteroid.estimated_diameter.feet.estimated_diameter_min) );
-			$tooltip.find('.max').text( Math.round(asteroid.estimated_diameter.feet.estimated_diameter_max) );
-			$tooltip.find('.vel').text( Math.round(asteroid.close_approach_data[0].relative_velocity.miles_per_hour) );
-			$tooltip.find('.distance').text( Math.round(asteroid.close_approach_data[0].miss_distance.miles) );
-
-			// Add our objects to the DOM.
-			$objects.append( $obj );
-			$tooltips.append( $tooltip );
-
-			// Give the asteroid a random spin for some visual interest.
-			TweenMax.set( $obj, { rotation: ~~( Math.random() * 360 ) } )
-			var tl = new TimelineMax({ repeat: -1 });
-			tl.to( $obj, 5 * velRatio , { rotation: Math.random() > 0.5 ? '+=360' : '-=360', ease: Power0.easeNone  } );
-
-
-		}
+		// Give the asteroid a spin for some visual interest.
+		// Speed of the spin is relative to the velocity of the asteroid. Direction is random.
+		TweenMax.set( $obj, { rotation: ~~( Math.random() * 360 ) } )
+		var tl = new TimelineMax({ repeat: -1 });
+		tl.to( $obj, 5 * velRatio , { rotation: Math.random() > 0.5 ? '+=360' : '-=360', ease: Power0.easeNone  } );
 
 	}
 
-	// Initialize our tooltips.
+	// Initialize our new tooltips.
 	$('.tooltip').tooltipster({ trigger: 'click'});
 
 }
@@ -185,8 +175,7 @@ function checkResults(data) {
 // Show the "No Results" section.
 function switchToNoResults() {
 	var tl = new TimelineMax();
-	tl.staggerTo(['.intro-text', '.form'], 1, { y: '-100vh', opacity: 0, ease: Back.easeIn }, 0.5);
-	tl.set(['.intro-text', '.form'], { display: 'none' });
+	tl.add( slideOutIntroAndForm() );
 	tl.set('.no-objects', { display: 'block', opacity: 0 });
 	tl.fromTo('.no-objects', 1, {y: 100, opacity: 0}, {y: 0, opacity: 1, ease: Power2.easeOut});
 }
@@ -194,8 +183,7 @@ function switchToNoResults() {
 // Show the error screen if we've bad results.
 function switchToError() {
 	var tl = new TimelineMax();
-	tl.staggerTo(['.intro-text', '.form'], 1, { y: '-100vh', opacity: 0, ease: Back.easeIn }, 0.5);
-	tl.set(['.intro-text', '.form'], { display: 'none' });
+	tl.add( slideOutIntroAndForm() );
 	tl.set('.error', { display: 'block', opacity: 0 });
 	tl.fromTo('.error', 1, {y: 100, opacity: 0}, {y: 0, opacity: 1, ease: Power2.easeOut});
 }
@@ -206,8 +194,16 @@ function switchToResults(data) {
 	createAsteroids(data);
 
 	var tl = new TimelineMax();
-	tl.staggerTo(['.intro-text', '.form'], 1, { y: '-100vh', opacity: 0, ease: Back.easeIn }, 0.5);
-	tl.set(['.intro-text', '.form'], { display: 'none' });
+	tl.add( slideOutIntroAndForm() );
 	tl.set('.objects-container', { display: 'block', opacity: 0 });
 	tl.fromTo('.objects-container', 1, {y: 100, opacity: 0}, {y: 0, opacity: 1, ease: Power2.easeOut});
+}
+
+// We do this repeatedly, so it's simpler to do it once in a function.
+// This returns a timeline, which we can add to our other timelines (timelineception?).
+function slideOutIntroAndForm() {
+	var tl = new TimelineMax();
+	tl.staggerTo(['.intro-text', '.form'], 1, { y: '-100vh', opacity: 0, ease: Back.easeIn }, 0.5);
+	tl.set(['.intro-text', '.form'], { display: 'none' });	
+	return tl;
 }
